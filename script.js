1d3c9ddddd4775c5abdc763f7b2f5b412b9de80a
enwadiogbu <emekanwadiogbu.jr@me.com>
2024-09-17 19:55:39 -0700

    project 1 of the 50 day Javascript project challenge by Brad Traversy Udemy course. created an image expanding card effect with html, css and vanilla javascript.

---
+++ script.js
@@ -0,0 +1,16 @@
+//Expanding effect
+
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active');
+    })
+}
